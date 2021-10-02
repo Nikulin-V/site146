d39f537cd25e2929cf32023500e17b251315ae1c
kvasoslave <kvasoslave@gmail.com>
2021-10-03 00:04:47 +0500

    не работает, но да

---
+++ site_app/school_plan.py
@@ -0,0 +1,10 @@
+#  Nikulin Vasily, Zhikharev Victor © 2021
+from flask import render_template
+
+from site_app import site
+
+
+@site.route('/school_plan')
+def school_plan():
+    return render_template("site/school_plan.html",
+                           title="План школы")
